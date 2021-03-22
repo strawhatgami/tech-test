9290083cafcc47632d0b20bde6b4bf1aab91ec17
Maxime Parriaux <maxime.parriaux@epitech.eu>
2021-03-22 13:43:46 +0100

    Add Beers !

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import Login from "./scopes/Login/Login";
 import "./App.css";
 import { AppProvider, useAppContext } from "./contexts/AppContext";
 import TokenHandler from "./scopes/TokenHandler/TokenHandler";
-import Trade from "./scopes/Trade/Trade";
+import Beers from "./scopes/Beers/Beers";
 
 // The famous nullable boolean we inherited from Java
 type nullableBoolean = boolean | null;
@@ -24,9 +24,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h5>{`Can't stop, won't stop,`}</h5>
         <img
-          src="https://cdn.worldvectorlogo.com/logos/gamestop.svg"
+          src="https://lh3.googleusercontent.com/proxy/gaAeBZiydOKQoCrfwFg_BgBUl14rRmeVatkaMchIdAkl7hvILA0MvjRCx2ANm1JAdzG4wfUxQX_4NlVELvug0Bl0HdC5cEr3MLs_KnNnXgV1DAwbATuP"
           className="App-logo"
           alt="logo"
         />
@@ -36,7 +35,7 @@ function App() {
           <Route path="/login" component={Login}></Route>
           {token && (
             <>
-              <Route path="/trade" component={Trade}></Route>
+              <Route path="/beers" component={Beers}></Route>
             </>
           )}
           <Route path="*" exact>
--- client/src/scopes/Login/Login.tsx
+++ client/src/scopes/Login/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     <div className="Login">
       <form
         onSubmit={() => {
-          history.push("/trade?token=test");
+          history.push("/beers?token=test");
         }}
       >
         <label htmlFor="username">Username</label>
